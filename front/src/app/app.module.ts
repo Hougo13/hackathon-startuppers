18f6c43bfc2e64713c04b4c4c706b69df583a423
Unknown <kinghyde111@gmail.com>
2018-11-10 05:14:09 +0100

    Feedback page in progress

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from "@angular/core";
 
 import { MatMenuModule } from "@angular/material/menu";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { AppRoutingModule } from "./app-routing.module";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { DataService } from "./data.service";
 import { AppComponent } from "./app.component";
 import { FeedbackComponent } from "./feedback/feedback.component";
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, FeedbackComponent],
@@ -17,7 +20,10 @@ import { FeedbackComponent } from "./feedback/feedback.component";
     AppRoutingModule,
     MatMenuModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatTableModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
--- front/src/app/feedback/feedback.component.ts
+++ front/src/app/feedback/feedback.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+export interface PeriodicData {
+  cardId: string;
+  hapiness: number;
+  cO2: number;
+  temperature: number;
+  acceleration: number;
+  decibel: number;
+}
+
+const ELEMENT_DATA: PeriodicData[] = [
+  {
+    cardId: "test",
+    hapiness: 2,
+    cO2: 1.0079,
+    temperature: 4,
+    acceleration: 1,
+    decibel: 9
+  }
+];
 
 @Component({
-  selector: 'app-feedback',
-  templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+  selector: "app-feedback",
+  templateUrl: "./feedback.component.html",
+  styleUrls: ["./feedback.component.css"]
 })
 export class FeedbackComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
+  displayedColumns: string[] = [
+    "cardId",
+    "hapiness",
+    "CO2",
+    "temperature",
+    "acceleration",
+    "decibel"
+  ];
+  dataSource = ELEMENT_DATA;
 }
