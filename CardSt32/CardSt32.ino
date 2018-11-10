5877e3a9269687307699cbb657ea1f749c602a2d
VAUBOURG Mandel <mandel.vaubourg@viacesi.fr>
2018-11-10 15:42:44 +0100

    complete arduino code

--- CardSt32/CardSt32.ino
+++ CardSt32/CardSt32.ino
@@ -1,16 +1,24 @@
 // Includes.
-#include <HTS221Sensor.h>  // Capteur Humidité et Température
 #include <WiFiST.h>        // WiFi de la carte IOT Node
 #include <PubSubClient.h>  //cette librairie permet d’envoyer et de recevoir des messages MQTT et de gérer le QoS
 #include <SPI.h>           // cette librairie vous permet de communiquer avec des périphériques SPI
-#include <LPS22HBSensor.h> // Capteur Barométrique
-#include <LIS3MDLSensor.h> // Magnétomètre ( boussolle digitale )
+#include <HTS221Sensor.h>  // Capteur Humidité et Température
 #include <LSM6DSLSensor.h> // 3D Gyroscope et Accelerométre)
-#define UID "005A694ed"
+
 #define I2C2_SCL PB10 // Port IC2 SCL utilisé sur la carte IOT Node
 #define I2C2_SDA PB11 // Port IC2 SDA utilisé sur la carte IOT Node
 #define SerialPort Serial
 #define INT1 PD11
+#define MG_PIN  A2     //define which analog input channel you are going to use
+#define BOOL_PIN A2
+#define DC_GAIN 3.5   //define the DC gain of amplifier
+#define READ_SAMPLE_INTERVAL 50    //define how many samples you are going to take in normal operation
+#define READ_SAMPLE_TIMES 8.5     //define the time interval(in milisecond) between each samples in 
+#define ZERO_POINT_VOLTAGE 0.226 //define the output of the sensor in volts when the concentration of CO2 is 400PPM
+#define REACTION_VOLTGAE 0.030 //define the voltage drop of the sensor when move the sensor from air into 1000ppm CO2
+#define SonPIN A5
+#define SEUIL_SON 80
+#define UID "005A694ed"
 #define TOKEN "i3RSj7HqWBY8O5LAiNL4" // Clé / Token généré dans le Device de Thingsboard
 
 // Update these with values suitable for your network.
@@ -24,6 +32,7 @@ const int threshold_piezzo = 100;
 // WiFi module setup
 SPIClass SPI_3(PC12, PC11, PC10);
 WiFiClass WiFi(&SPI_3, PE0, PE1, PE8, PB13);
+float CO2Curve[3]  =  {2.602,ZERO_POINT_VOLTAGE,(REACTION_VOLTGAE/(2.602-3))};   
 WiFiClient STClient;
 
 int status = WL_IDLE_STATUS; // the Wifi  status
@@ -46,14 +55,14 @@ float humidity;     //en %
 float temperature;  //en °C
 bool noise;         //True si bruit
 int loudness = 0;
+int tauxCo2 = 0;
 
 void INT1Event();
 void setup()
 {
-
-    // Led.
-    pinMode(LED_BUILTIN, OUTPUT);
-
+    pinMode(BOOL_PIN, INPUT); //set pin to input
+    pinMode(SonPIN, INPUT);
+    digitalWrite(BOOL_PIN, HIGH); //turn on pullup resistors
     // Initialize serial for output.
     Serial.begin(115200); // démarage de la connection serie à la vitesse de 115200 bauds
 
@@ -83,7 +92,6 @@ void setup()
 
 void setup_wifi()
 {
-
     delay(100);
 
     // Initialize the WiFi module:
@@ -91,8 +99,7 @@ void setup_wifi()
     {
         Serial.println("WiFi module not detected");
         // don't continue:
-        while (true)
-            ;
+        while (true);
     }
 
     // Attempt to connect to Wifi network:
@@ -102,13 +109,11 @@ void setup_wifi()
     while (status != WL_CONNECTED)
     {
         Serial.print(status);
-
         // Connect to network:
         status = WiFi.begin(ssid, password);
         // Wait 10 seconds for connection:
         delay(10000);
     }
-
     Serial.println();
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
@@ -158,24 +163,28 @@ void loop()
     }
     client.loop();
 
+    //bloque 4500ms
+    noise = getNoise();
+    //bloque500ms
+    float volts = MGRead(BOOL_PIN);
+    tauxCo2 = MGGetPercentage(volts, CO2Curve);
     if (millis() - lastSend > 1000)
     { // Update and send only after x seconds
         getAndSendData();
         lastSend = millis();
     }
-    delay(5000); // delais entre chaque boucle ( ici 55 sec)
 }
 
 // Génération et envoi des données
 
 void getAndSendData()
 {
-
     Serial.println("Collecting datas...");
     Serial.println("");
 
     HumTemp->GetHumidity(&humidity); // récupération des données
     HumTemp->GetTemperature(&temperature);
+    AccGyr->Disable_X();
 
     Serial.println(" ");
     Serial.print("==> Sent Payload Listing  -> ");
@@ -198,11 +207,13 @@ void getAndSendData()
     payload += ",";
     payload += "\"acceleration\":";
     payload += acceleration;
+    payload += ",\"tauxCo2\":";
+    payload += tauxCo2;
     payload += "}";
 
     // Send payload
-    char attributes[100];
-    payload.toCharArray(attributes, 100);
+    char attributes[128];
+    payload.toCharArray(attributes, 128);
     client.publish("v1/devices/me/telemetry", attributes);
     Serial.print("Payload ");
     Serial.println(attributes);
@@ -222,4 +233,35 @@ void INT1Event()
     AccGyr->Get_X_Axes(accelerometer);
     acceleration = sqrt(accelerometer[0] * accelerometer[0] + accelerometer[1] * accelerometer[1] + accelerometer[2] * accelerometer[2]);
     acceleration = (acceleration > accOld ? acceleration : accOld);
+}
+
+float MGRead(int mg_pin)
+{
+    int i;
+    float v=0;
+
+    for (i=0;i<50;i++) {
+        v += analogRead(mg_pin);
+        delay(10);
+    }
+    v = (v/50) *5/1024;
+    return v;  
+}
+
+int MGGetPercentage(float volts, float *pcurve)
+{
+   if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
+      return -1;
+   } else { 
+      return pow(10, ((volts/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
+   }
+}
+
+bool getNoise(){
+  int sound_overflow= 0;
+  for(int i=0;i<450;i++){
+    if(analogRead(SonPIN) < SEUIL_SON) ++sound_overflow;
+    delay(10);
+  }
+  return sound_overflow > 120;
 }
